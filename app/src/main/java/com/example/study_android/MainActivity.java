5514dc0cc76cc647b9df998fa560a0c0e95d07ac
sleepygloa <sleepygloa@gmail.com>
2020-01-22 00:28:59 +0900

    안드로이드공부(상기) Intent onActivityResult

--- app/src/main/java/com/example/study_android/MainActivity.java
+++ app/src/main/java/com/example/study_android/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.study_android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    int REQUEST_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //명시적 Intent
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        intent.putExtra("INTENT_KEY", 5);
+        //startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE);
 
+        //암시적 Intent
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https:google.com"));
+//        startActivity(intent);
 
     }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == 200){
+                String result = data.getStringExtra("RESULT");
+                Log.d("onActivityResult", "result : " + result);
+            }else if(resultCode == 300){
+            }
+
+        }
+  }
+
 /*
     @Override
     protected  void onSaveInstanceState(Bundle outState){
--- app/src/main/java/com/example/study_android/SecondActivity.java
+++ app/src/main/java/com/example/study_android/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.study_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,8 +11,20 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Lifecycle", "2 : onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //명시적 Intent
+//        Intent intent = getIntent();
+//        int data = intent.getIntExtra("INTENT_KEY", 0);
+//        Log.d("INTENT_KEY", ""+data);
+
+        Intent intent = new Intent();
+        intent.putExtra("RESULT", "성공");
+        setResult(200, intent);
+        finish();
+
     }
 
 
